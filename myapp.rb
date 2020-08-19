25c38ca249945c89c94d6487d21e1b90cf36339e
Takanori Ohashi <ohashi_t@uuum.jp>
2020-08-19 15:42:19 +0900

    updated

--- myapp.rb
+++ myapp.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
- 
+require 'slack-ruby-client'
+
+
+Slack.configure do |config|
+  config.token = "xoxb-2551775506-1307095574436-ybSSzfG1TVOPWyxweg3hAiGz"
+end
+
+client = Slack::Web::Client.new
+client.auth_test
+
 get '/' do
     "Hello World!"
 end
@@ -10,5 +19,11 @@ end
 post '/webhook' do
   channel_id = params[:channel_id]
   command = params[:command]
-  test = "api_testtest"
+  text = params[:text]
+  case command
+  when "/vim" then
+    client.chat_postMessage(channel: '#x-test', text: 'Hello World', as_user: true)
+  end
+  
+"good"
 end
