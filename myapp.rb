5771b323b05bc92d27e41d0f2fdde23a11b31870
Takanori Ohashi <ohashi_t@uuum.jp>
2020-08-19 16:04:42 +0900

    modified

--- myapp.rb
+++ myapp.rb
@@ -5,7 +5,7 @@ require 'slack-ruby-client'
 
 
 Slack.configure do |config|
-  config.token = "xoxb-2551775506-1307095574436-K96UT6n6Bd13RWbrdrgV9aMq"
+  config.token = "xoxb-2551775506-1307095574436-lZK83CXVB8ciOQc740MFsUe7"
 end
 
 client = Slack::Web::Client.new
